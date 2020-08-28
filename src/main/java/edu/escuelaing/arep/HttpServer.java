33c09f73f184903a5406c138bbe35809bc4420a9
Diego Puerto <diego23p@hotmail.com>
2020-08-28 15:59:55 -0500

    Lambda

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -86,89 +86,113 @@ public class HttpServer {
      */
   public static void returnRequest(String req) throws IOException {
 	  
-	  String path = "src/main/resources/";
-      String ext = FilenameUtils.getExtension(req);
-      System.out.println("Extencion: "+ext);
-      
-      if (ext.equals("jpg") || ext.equals("png")) {
-    	  path+="img/";
-      }
-      
-      if (req.equals("Tabla.html")) {
-    	  System.out.println("Ruta del recurso: Recurso no estatico, llamado a base de datos");
-      } else {
-    	  System.out.println("Ruta del recurso: "+path+req);
-      }
-      
-      File file = new File(path+req);
-
-	  if (req.equals("Tabla.html")) {
+	  if(req.startsWith("ValorLambda")) {
+		  String[] partes = req.split("\\?");
+		  String valor = partes[1];
 		  out.println("HTTP/1.1 200 \r\nContent-Type: text/html\r\n\r\n");
-    	  
-    	  String outputLine = 
-    	          "<!DOCTYPE html>" + 
-    	          "<html>" + 
-    	          "<head>" + 
-    	          "<meta charset=\"UTF-8\">" + 
-    	          "<title>Base de Datos</title>\n" + 
-    	          "</head>" + 
-    	          "<body>" + 
-    	          "<h1>Tabla Animales</h1>" + 
-    	          "<table border=\"1\">"+
-    	          "<tr>"+
-    	          "<td>Id</td>"+
-    	          "<td>Animal</td>"+
-    	          "<td>Nombre</td>"+
-    	          "</tr>";
-    	     
-    	          ArrayList<Animal> lista = conexion.getEstudiantes();
-    	          for (int i=0;i<lista.size();i++)
-    	          {
-    	             outputLine=outputLine +"<tr>"+
-	    	            		 				"<td>"+lista.get(i).getId()+"</td>"+
-	    	            		 				"<td>"+lista.get(i).getAnimal()+"</td>"+
-							    	            "<td>"+lista.get(i).getNombre()+"</td>"+
-						    	            "</tr>";
-    	          }
-    	          
-    	          outputLine=outputLine +"</table>"+
-    	          "</body>" + 
-    	          "</html>"; 
-    	    out.println(outputLine);
-      } else if (file.exists() && !file.isDirectory()) {
+		  
+		  Get expresion = (n) -> { 
+	    	  out.println(
+	    	  "<!DOCTYPE html>"+
+	    	  "<html>"+
+	    	  	"<head>"+
+	    	  		"<meta charset=\"UTF-8\">"+
+	    	  		"<title>File</title>"+
+	    	  	"</head>"+
+	    	  	"<body>"+
+	    	  		"<h1> Bienvenido al programa lambda, el valor que ingreso fue: "+ n +"</h1>"+
+	    	  	"</body>"+
+	    	  "</html>" );
+	      };
+	      
+	      expresion.get(valor);
+		  
+	  } else {
+	  
+		  String path = "src/main/resources/";
+	      String ext = FilenameUtils.getExtension(req);
+	      System.out.println("Extencion: "+ext);      	    		  
+	    		  
 	      if (ext.equals("jpg") || ext.equals("png")) {
-	    	  	
-				FileInputStream fis = new FileInputStream(file);
-				byte[] data = new byte[(int) file.length()];
-				fis.read(data);
-				fis.close();
-	                      
-	            // Cabeceras con la info de imagen (ya sea png o jpg)
-				DataOutputStream binaryOut = new DataOutputStream(clientSocket.getOutputStream());
-				binaryOut.writeBytes("HTTP/1.0 200 OK\r\n");
-				binaryOut.writeBytes("Content-Type: image/"+ext+"\r\n");
-				binaryOut.writeBytes("Content-Length: " + data.length);
-				binaryOut.writeBytes("\r\n\r\n");
-				binaryOut.write(data);
-	
-				binaryOut.close();
-	    	  
-	      } else {
-	    	  out.println("HTTP/1.1 200 \r\nContent-Type: text/html\r\n\r\n");
-	    	  BufferedReader br = new BufferedReader(new FileReader(file));
-	    	  StringBuilder stringBuilder = new StringBuilder();
-	          String st;
-	          
-	          while ((st = br.readLine()) != null) {
-	              stringBuilder.append(st);
-	          }
-	          
-	          out.println(stringBuilder.toString());
-	          br.close();
+	    	  path+="img/";
 	      }
-      } else {
-    	  out.println("HTTP/1.1 404 \r\n\r\n<html><body><h1>ERROR 404: NOT FOUND</h1></body></html>");
-      }
+	      
+	      if (req.equals("Tabla.html")) {
+	    	  System.out.println("Ruta del recurso: Recurso no estatico, llamado a base de datos");
+	      } else {
+	    	  System.out.println("Ruta del recurso: "+path+req);
+	      }
+	      
+	      File file = new File(path+req);
+	
+		  if (req.equals("Tabla.html")) {
+			  out.println("HTTP/1.1 200 \r\nContent-Type: text/html\r\n\r\n");
+	    	  
+	    	  String outputLine = 
+	    	          "<!DOCTYPE html>" + 
+	    	          "<html>" + 
+	    	          "<head>" + 
+	    	          "<meta charset=\"UTF-8\">" + 
+	    	          "<title>Base de Datos</title>\n" + 
+	    	          "</head>" + 
+	    	          "<body>" + 
+	    	          "<h1>Tabla Animales</h1>" + 
+	    	          "<table border=\"1\">"+
+	    	          "<tr>"+
+	    	          "<td>Id</td>"+
+	    	          "<td>Animal</td>"+
+	    	          "<td>Nombre</td>"+
+	    	          "</tr>";
+	    	     
+	    	          ArrayList<Animal> lista = conexion.getEstudiantes();
+	    	          for (int i=0;i<lista.size();i++)
+	    	          {
+	    	             outputLine=outputLine +"<tr>"+
+		    	            		 				"<td>"+lista.get(i).getId()+"</td>"+
+		    	            		 				"<td>"+lista.get(i).getAnimal()+"</td>"+
+								    	            "<td>"+lista.get(i).getNombre()+"</td>"+
+							    	            "</tr>";
+	    	          }
+	    	          
+	    	          outputLine=outputLine +"</table>"+
+	    	          "</body>" + 
+	    	          "</html>"; 
+	    	    out.println(outputLine);
+	      } else if (file.exists() && !file.isDirectory()) {
+		      if (ext.equals("jpg") || ext.equals("png")) {
+		    	  	
+					FileInputStream fis = new FileInputStream(file);
+					byte[] data = new byte[(int) file.length()];
+					fis.read(data);
+					fis.close();
+		                      
+		            // Cabeceras con la info de imagen (ya sea png o jpg)
+					DataOutputStream binaryOut = new DataOutputStream(clientSocket.getOutputStream());
+					binaryOut.writeBytes("HTTP/1.0 200 OK\r\n");
+					binaryOut.writeBytes("Content-Type: image/"+ext+"\r\n");
+					binaryOut.writeBytes("Content-Length: " + data.length);
+					binaryOut.writeBytes("\r\n\r\n");
+					binaryOut.write(data);
+		
+					binaryOut.close();
+		    	  
+		      } else {
+		    	  out.println("HTTP/1.1 200 \r\nContent-Type: text/html\r\n\r\n");
+		    	  BufferedReader br = new BufferedReader(new FileReader(file));
+		    	  StringBuilder stringBuilder = new StringBuilder();
+		          String st;
+		          
+		          while ((st = br.readLine()) != null) {
+		              stringBuilder.append(st);
+		          }
+		          
+		          out.println(stringBuilder.toString());
+		          br.close();
+		      }
+	      } else {
+	    	  out.println("HTTP/1.1 404 \r\n\r\n<html><body><h1>ERROR 404: NOT FOUND</h1></body></html>");
+	      }
+	  }
   }
   
   static int getPort() {
